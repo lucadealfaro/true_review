e59959bb885c438f3e354d379c658c51380f953a
Luca de Alfaro <luca@dealfaro.org>
2016-10-18 17:57:27 -0700

    more work on the js

--- static/js/topic_index.js
+++ static/js/topic_index.js
@@ -4,16 +4,50 @@ var app = function() {
 
     Vue.config.silent = false; // show all warnings
 
+    // Sortable fields in table.
+    var sortable = ['title', 'score', 'num_reviews'];
+
+    function reset_sort() {
+        for (var i = 0; i < sortable.length; i++) {
+            self.vue.is_sort_up[sortable[i]] = false;
+            self.vue.is_sort_down[sortable[i]] = false;
+        }
+    }
+
+    self.toggle_sort = function (col) {
+        // Toggle the sort for a given column.
+        // The default is to sort down (from largest to smallest).
+        var is_down = self.vue.is_sort_down[col];
+        reset_sort();
+        if (is_down) {
+            self.vue.is_sort_up[col] = true;
+        } else {
+            self.vue.is_sort_down[col] = true;
+        }
+        self.get_papers();
+    };
+
+    function get_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx,
+            primary_papers: self.vue.primary_papers
+        };
+        for (var i = 0; i < sortable.length; i++) {
+            var k = sortable[i];
+            if (self.vue.is_sort_up[k]) {
+                pp['sort_' + k] = 'up';
+            } else if (self.vue.is_sort_down[k]) {
+                pp['sort_' + k] = 'down';
+            }
+        }
+        return "/api/topic_papers/" + topic_id + "?" + $.param(pp);
+    }
+
     // Function to get the data.
-    self.get_data = function () {
-        var url = "/api/topic_papers/" + topic_id + "?";
-        url += $.param({
-            requested_num_papers: self.vue.requested_num_papers,
-            primary_papers: self.vue.primary_papers,
-            sort_by: self.vue.sort_by,
-            sort_descending: self.vue.sort_descending
-        });
-        $.getJSON(url, function (data) {
+    self.get_papers = function () {
+        $.getJSON(get_url(0, 20), function (data) {
+            self.vue.has_more = data.has_more;
             self.vue.papers = data.papers;
             self.vue.can_review = data.can_review;
             self.vue.can_add_paper = data.can_add_paper;
@@ -27,8 +61,17 @@ var app = function() {
     };
     self.set_primary = function(b) {
         self.vue.primary_papers = b;
-        // TODO: Reload data.
+        self.get_papers();
     };
+
+    self.get_more_papers = function () {
+        var num_papers = self.vue.papers.length;
+        $.getJSON(get_url(num_papers + 1, num_papers + 21), function (data) {
+            self.vue.has_more = data.has_more;
+            self.vue.papers.push(data.papers);
+        });
+    };
+
 
     self.vue = new Vue({
         el: "#topics-div",
@@ -36,20 +79,23 @@ var app = function() {
         unsafeDelimiters: ['!{', '}'],
         data: {
             papers: [],
-            requested_num_papers: 20,
             primary_papers: true,
             show_paper_scores: false,
             can_review: false,
             can_add_paper: false,
-            sort_by: "score",
-            sort_descending: true
+            has_more: false,
+            is_sort_up: {'title': false, 'score': false, 'num_reviews': false},
+            is_sort_down: {'title': false, 'score': true, 'num_reviews': false}
         },
         methods: {
-            set_scores: self.set_scores
+            set_scores: self.set_scores,
+            set_primary: self.set_primary,
+            toggle_sort: self.toggle_sort,
+            get_more_papers: self.get_more_papers
         }
     });
 
-    self.get_data();
+    self.get_papers();
     $("#topics-div").show();
 
     return self;
